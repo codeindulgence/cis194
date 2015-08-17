d44eb04a8742204539238a33e8197fadf9b9cc30
Nick Butler <nick@codeindulgence.com>
2015-08-17 12:17:24 -1000

    Week 2: parseMessage

---
+++ 02-ADTs/LogAnalysis.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage string = case words string of
+  ("I":t:msg)          -> LogMessage Info (read t) (unwords msg)
+  ("W":t:msg)          -> LogMessage Warning (read t) (unwords msg)
+  ("E":t:severity:msg) -> LogMessage (Error (read severity)) (read t) (unwords msg)
+  _                    -> Unknown string
