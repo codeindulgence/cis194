b8a9f35d47b3b0f28fb2617a60ef0f7d61a1bf43
Nick Butler <nick@codeindulgence.com>
2015-08-18 21:15:45 -1000

    Week 2: insert

--- 02-ADTs/LogAnalysis.hs
+++ 02-ADTs/LogAnalysis.hs
@@ -13,3 +13,10 @@ parseMessage string = case words string of
 
 parse :: String -> [LogMessage]
 parse string = map parseMessage $ lines string
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert message Leaf = Node Leaf message Leaf
+insert m1@(LogMessage _ t1 _) (Node l m2@(LogMessage _ t2 _) r)
+  | t1 > t2   = Node l m2 (insert m1 r)
+  | otherwise = Node (insert m1 l) m2 r
+insert _ tree = tree
