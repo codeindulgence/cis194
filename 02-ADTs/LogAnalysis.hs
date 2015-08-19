f83c28b1a958c1186590bcc2605fc34b09a1e627
Nick Butler <nick@codeindulgence.com>
2015-08-18 21:53:09 -1000

    Week 2: build

--- 02-ADTs/LogAnalysis.hs
+++ 02-ADTs/LogAnalysis.hs
@@ -20,3 +20,20 @@ insert m1@(LogMessage _ t1 _) (Node l m2@(LogMessage _ t2 _) r)
   | t1 > t2   = Node l m2 (insert m1 r)
   | otherwise = Node (insert m1 l) m2 r
 insert _ tree = tree
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+testMessages :: [LogMessage]
+testMessages = [
+  LogMessage Info 6 "Completed armadillo processing",
+  LogMessage Info 1 "Nothing to report",
+  LogMessage Info 4 "Everything normal",
+  LogMessage Info 11 "Initiating self-destruct sequence",
+  LogMessage (Error 3) 70 "Way too many pickles",
+  LogMessage (Error 8) 65 "Bad pickle-flange interaction detected",
+  LogMessage Warning 5 "Flange is due for a check-up",
+  LogMessage Info 7 "Out for lunch, back in two time steps",
+  LogMessage (Error 2) 20 "Too many pickles",
+  LogMessage Info 9 "Back from lunch",
+  LogMessage (Error 10) 99 "Flange failed!"]
